23deb5c6033e9ef23c83880061c16f128c05e807
Leidy Quiñones <lalis.181014@gmail.com>
2023-06-15 14:21:57 -0500

    Código

--- routes/customers.js
+++ routes/customers.js
@@ -1,57 +1,32 @@
-const {Customer, validate} = require('../models/customer'); 
+const Joi = require('joi');
+const bcrypt = require('bcrypt');
+const _ = require('lodash');
+const {User} = require('../models/user');
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
-
-router.get('/', async (req, res) => {
-  const customers = await Customer.find().sort('name');
-  res.send(customers);
-});
 
 router.post('/', async (req, res) => {
   const { error } = validate(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
 
-  let customer = new Customer({ 
-    name: req.body.name,
-    isGold: req.body.isGold,
-    phone: req.body.phone
-  });
-  customer = await customer.save();
-  
-  res.send(customer);
+  let user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send('Invalid email or password.');
+
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) return res.status(400).send('Invalid email or password.');
+
+  const token = user.generateAuthToken();
+  res.send(token);
 });
 
-router.put('/:id', async (req, res) => {
-  const { error } = validate(req.body); 
-  if (error) return res.status(400).send(error.details[0].message);
+function validate(req) {
+  const schema = {
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(5).max(255).required()
+  };
 
-  const customer = await Customer.findByIdAndUpdate(req.params.id,
-    { 
-      name: req.body.name,
-      isGold: req.body.isGold,
-      phone: req.body.phone
-    }, { new: true });
-
-  if (!customer) return res.status(404).send('The customer with the given ID was not found.');
-  
-  res.send(customer);
-});
-
-router.delete('/:id', async (req, res) => {
-  const customer = await Customer.findByIdAndRemove(req.params.id);
-
-  if (!customer) return res.status(404).send('The customer with the given ID was not found.');
-
-  res.send(customer);
-});
-
-router.get('/:id', async (req, res) => {
-  const customer = await Customer.findById(req.params.id);
-
-  if (!customer) return res.status(404).send('The customer with the given ID was not found.');
-
-  res.send(customer);
-});
+  return Joi.validate(req, schema);
+}
 
 module.exports = router; 
--- startup/db.js
+++ startup/db.js
@@ -1,7 +1,6 @@
 const winston = require('winston');
 const mongoose = require('mongoose');
-const config=require("config");
 module.exports= function(){
-    mongoose.connect(config.get("db"),{useNewUrlParser:true,  useCreateIndex: true,useUnifiedTopology: true})
+    mongoose.connect('mongodb://localhost/cinema',{useNewUrlParser:true,  useCreateIndex: true,useUnifiedTopology: true})
   .then(() => winston.info('Connected to MongoDB...'))
 }
