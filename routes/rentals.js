173542132dcba29d319c9a611e871f3151f34a55
Leidy Quiñones <lalis.181014@gmail.com>
2023-06-18 19:30:13 -0500

    se cambia dependencia fawn

--- routes/rentals.js
+++ routes/rentals.js
@@ -1,12 +1,10 @@
-const {Rental, validate} = require('../models/rental'); 
-const {Movie} = require('../models/movie'); 
-const {Customer} = require('../models/customer'); 
+const { Rental, validate } = require('../models/rental');
+const { Movie } = require('../models/movie');
+const { Customer } = require('../models/customer');
 const mongoose = require('mongoose');
-const Fawn = require('fawn');
+const { startSession } = require('mongoose');
 const express = require('express');
 const router = express.Router();
-
-Fawn.init(mongoose);
 
 router.get('/', async (req, res) => {
   const rentals = await Rental.find().sort('-dateOut');
@@ -14,42 +12,46 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  const { error } = validate(req.body); 
+  const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  const customer = await Customer.findById(req.body.customerId);
-  if (!customer) return res.status(400).send('Invalid customer.');
-
-  const movie = await Movie.findById(req.body.movieId);
-  if (!movie) return res.status(400).send('Invalid movie.');
-
-  if (movie.numberInStock === 0) return res.status(400).send('Movie not in stock.');
-
-  let rental = new Rental({ 
-    customer: {
-      _id: customer._id,
-      name: customer.name, 
-      phone: customer.phone
-    },
-    movie: {
-      _id: movie._id,
-      title: movie.title,
-      dailyRentalRate: movie.dailyRentalRate
-    }
-  });
+  const session = await startSession();
+  session.startTransaction();
 
   try {
-    new Fawn.Task()
-      .save('rentals', rental)
-      .update('movies', { _id: movie._id }, { 
-        $inc: { numberInStock: -1 }
-      })
-      .run();
-  
+    const customer = await Customer.findById(req.body.customerId).session(session);
+    if (!customer) return res.status(400).send('Invalid customer.');
+
+    const movie = await Movie.findById(req.body.movieId).session(session);
+    if (!movie) return res.status(400).send('Invalid movie.');
+
+    if (movie.numberInStock === 0) return res.status(400).send('Movie not in stock.');
+
+    let rental = new Rental({
+      customer: {
+        _id: customer._id,
+        name: customer.name,
+        phone: customer.phone
+      },
+      movie: {
+        _id: movie._id,
+        title: movie.title,
+        dailyRentalRate: movie.dailyRentalRate
+      }
+    });
+
+    await rental.save({ session });
+
+    movie.numberInStock--;
+    await movie.save({ session });
+
+    await session.commitTransaction();
     res.send(rental);
-  }
-  catch(ex) {
+  } catch (ex) {
+    await session.abortTransaction();
     res.status(500).send('Something failed.');
+  } finally {
+    session.endSession();
   }
 });
 
@@ -61,4 +63,4 @@ router.get('/:id', async (req, res) => {
   res.send(rental);
 });
 
-module.exports = router; 
+module.exports = router;
